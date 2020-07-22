cc0c6b0f73e596a09a86664d6e2bf08467308c9c
Tanaes <jonsan@gmail.com>
2020-07-22 16:39:03 -0400

    setting up structure

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name='opentrons_functions',
+    version='0.1.0',    
+    description='A collection of functions compatible with the Opentrons API',
+    url='https://github.com/tanaes/opentrons_functions',
+    author='Jon G Sanders',
+    author_email='jonsan@gmail.com',
+    license='MIT License',
+    packages=find_packages(),
+    install_requires=['numpy',                     
+                      ],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',  
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: MacOS :: MacOS X'
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6'
+    ],
+)
