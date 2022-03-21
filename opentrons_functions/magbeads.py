1934144b45bea9d9d77212f3df49ec571b8ea8c9
Tanaes <jonsan@gmail.com>
2022-03-21 12:10:30 -0400

    added tip touch in bead mix

--- opentrons_functions/magbeads.py
+++ opentrons_functions/magbeads.py
@@ -22,8 +22,9 @@ def bead_mix(pipette,
                              plate[col].bottom(z=z_offset + mix_lift))
 
         pipette.blow_out(plate[col].top())
+        pipette.touch_tip()
         pipette.aspirate(20)
-        
+
         if drop_tip:
             pipette.drop_tip()
         else:
